53b6bb6539c6153641bcf1acf3d4f123446c091d
mohammad blur <blor1379@gmail.com>
2024-01-07 22:04:44 +0330

    Initial Commit

---
+++ hiAnimation/hiAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiAnimationApp.swift
+//  hiAnimation
+//
+//  Created by Mohammad Blur on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct hiAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
